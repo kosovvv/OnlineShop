49d450a9d5fc8c9043daeb0eb7586c8c433d0d89
Emil Kosov <emilkosov02@hotmail.com>
2024-03-29 00:01:25 +0200

    delete

--- Skinet/NewServer/Data/AspNetCoreTemplate.Data.Models/ProductBrand.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OnlineShop.Data.Models;
-
-using AspNetCoreTemplate.Data.Common.Models;
-
-public class ProductBrand : BaseDeletableModel<int>
-{
-    public string Name { get; set; }
-}
--- Skinet/NewServer/Data/AspNetCoreTemplate.Data.Models/ProductType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OnlineShop.Data.Models;
-
-using AspNetCoreTemplate.Data.Common.Models;
-
-public class ProductType : BaseDeletableModel<int>
-{
-    public string Name { get; set; }
-}
