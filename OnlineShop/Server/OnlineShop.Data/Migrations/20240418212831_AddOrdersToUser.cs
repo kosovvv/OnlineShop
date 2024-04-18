fe3daee441f66f0eb00f661bdb17d467a3615e7e
Emil Kosov <emilkosov02@hotmail.com>
2024-04-19 02:09:32 +0300

    add migration

---
+++ OnlineShop/Server/OnlineShop.Data/Migrations/20240418212831_AddOrdersToUser.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineShop.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrdersToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductBrands_ProductBrandId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypeId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "BuyerEmail",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductTypeId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductBrandId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BuyerId",
+                table: "Orders",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_BuyerId",
+                table: "Orders",
+                column: "BuyerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AspNetUsers_BuyerId",
+                table: "Orders",
+                column: "BuyerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductBrands_ProductBrandId",
+                table: "Products",
+                column: "ProductBrandId",
+                principalTable: "ProductBrands",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypeId",
+                table: "Products",
+                column: "ProductTypeId",
+                principalTable: "ProductTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AspNetUsers_BuyerId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductBrands_ProductBrandId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypeId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_BuyerId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "BuyerId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductTypeId",
+                table: "Products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductBrandId",
+                table: "Products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BuyerEmail",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductBrands_ProductBrandId",
+                table: "Products",
+                column: "ProductBrandId",
+                principalTable: "ProductBrands",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypeId",
+                table: "Products",
+                column: "ProductTypeId",
+                principalTable: "ProductTypes",
+                principalColumn: "Id");
+        }
+    }
+}
